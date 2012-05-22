ac7b2c232a9b9492a5d8a0ed7d47e80887bbffcc
Zhihao Jia <dugushuiyi@gmail.com>
2012-05-21 22:05:42 -0400

    add impilicit cast testcase

--- test/Parser/implicit-casts.c
+++ test/Parser/implicit-casts.c
@@ -1,15 +1,19 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -pedantic %s
 _Complex double X;
+_nan int Y;
 void test1(int c) {
   X = 5;
+  Y = 6;
 }
 void test2() {
   int i;
   double d = i;
   double _Complex a = 5;
-
+  _nan int b = 6;
+  
   test1(a);
   a = 5;
+  b = 6;
   d = i;
 }
 int test3() {
